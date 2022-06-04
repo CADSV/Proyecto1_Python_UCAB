bfbda8dca8ea34291854bdc00d6dccc9d15a9cb2
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-06-04 12:02:11 -0400

    Creada la excepcion cuando se ingresa una opcion numerica inexistente en un menu, y probado el diseño del menu principal del programa

--- views/main_menu.py
+++ views/main_menu.py
@@ -1,17 +1,25 @@
 # Importacions de librerias necesarias
-from views.utils import create_menu
-# from views.utils import create_menu, clear_screen, call_exit
+from views.utils import (
+    create_menu, #Para crear el menu
+    clear_screen #Para limpiar la pantalla
+    )
+from exceptions.menu_exceptions import InexistentMenuOptionError #Excepcion de opcion invalida en el menu
 # from views.files import file_menu
 # from views.actions import actions_menu
 
 # Funcion encargada de mostrar el menu principal
-def main(context: dict) -> dict:
+def main():
+    context = None
     selected_option = None
     main_menu_options = ["Archivo", "Acciones", "Salir"]
 
     while(True):
         try: 
-            # clear_screen()
+            clear_screen()
+
+            print('\n')
+            print('Bienvenidos al primer proyecto de Python')
+            print('Desarrollado por: Calos Doffiny S-V.  CI: V-27.814.707')
 
             create_menu(main_menu_options, "Menu Principal")
 
@@ -29,12 +37,12 @@ def main(context: dict) -> dict:
                 # call_exit()
                 print('333')
             else:
-                raise InexistentMenuOptionError("La opción seleccionada no existe")
+                raise InexistentMenuOptionError("¡ERORR! La opción seleccionada no existe. Por favor seleccione una opción válida :(")
         
         except ValueError:
-            print("\nEl valor ingresado no es valido, por favor intente nuevamente con un número entero\n")	
+            print("\n\n¡ERROR! El valor ingresado no es valido, por favor intente nuevamente con un número entero :(\n")	
             input("Por favor presione cualquier tecla para continuar...")
         
         except (InexistentMenuOptionError) as e:
-            print(f'\n{e}\n')
+            print(f'\n\n{e}\n')
             input("Por favor presione cualquier tecla para continuar...")
--- views/utils.py
+++ views/utils.py
@@ -1,14 +1,18 @@
 # Importaciones de librerias necesarias
-import os #Para trabajar con el sistema operativo
+import os #Para trabajar con el sistema operativo de la computadora desde la cual se esta ejecutando el programa
 import sys
 
 # Funcion encargada de mostrar todos los menus de la aplicacion
-def create_menu(menu_options: list[str], title: str) -> dict:
+def create_menu(menu_options: list[str], title: str):
     print("\n")
-    print(title)
-    for i in range(30):
+    print(f'            {title}')
+    for i in range(20):
         print("**", end="")
     print("\n")
     for i in range(len(menu_options)):
         print(f"  {i+1}. {menu_options[i]}")
     print("\n\n")
+
+# Funcion encargada de limpiar la pantalla
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear") #Para limpiar la pantalla tanto en windows como en linux
