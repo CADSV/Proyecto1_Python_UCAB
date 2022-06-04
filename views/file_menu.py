2170f87f3c342b3b5f7ee2a52b424b2190ebc777
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-06-04 13:39:29 -0400

    Agregado el menu de carga de archivos

--- views/file_menu.py
+++ views/file_menu.py
@@ -8,6 +8,7 @@ from views.utils import (
 from exceptions.menu_exceptions import InexistentMenuOptionError #Excepcion de opcion invalida en el menu
 
 
+# Funcion encargada de mostrar el menu de los archivos
 def file_menu(context: dict) -> dict:
     selected_option = None
     main_menu_options = ["Cargar un archivo", "Volver al Menú Principal", "Salir del Programa"]
@@ -21,13 +22,53 @@ def file_menu(context: dict) -> dict:
             selected_option = int(input("Por favor seleccione una opción: "))
 
             if (selected_option == 1):
-                # file_menu(context)
+                load_file_menu(context)
                 print('111')
 
             elif (selected_option == 2):
                 break
 
             elif (selected_option == 3):
+                call_exit()
+            else:
+                raise InexistentMenuOptionError("¡ERORR! La opción seleccionada no existe. Por favor seleccione una opción válida :(")
+        
+        except ValueError:
+            print("\n\n¡ERROR! El valor ingresado no es valido, por favor intente nuevamente con un número entero :(\n")	
+            input("Por favor presione cualquier tecla para continuar...")
+        
+        except (InexistentMenuOptionError) as e:
+            print(f'\n\n{e}\n')
+            input("Por favor presione cualquier tecla para continuar...")
+
+    return context
+
+
+# Funcion encargada de mostrar el menu de cargar un archivo
+def load_file_menu(context: dict) -> dict:
+    selected_option = None
+    main_menu_options = ["Cargar el archivo por defecto competencia.tx", "Cargar un archivo propio", "Volver al Menú de Archivos", "Salir del Programa"]
+
+    while(True):
+        try: 
+            clear_screen()
+
+            create_menu(main_menu_options, "         MENÚ DE CARGAR ARCHIVOS")
+
+            selected_option = int(input("Por favor seleccione una opción: "))
+
+            if (selected_option == 1):
+                # load_file(context)
+                print('111')
+
+            elif (selected_option == 2):
+                # load_file(context)
+                print('222')
+
+            elif (selected_option == 3):
+                break
+
+            elif (selected_option == 4):
                 call_exit()
             else:
                 raise InexistentMenuOptionError("¡ERORR! La opción seleccionada no existe. Por favor seleccione una opción válida :(")
